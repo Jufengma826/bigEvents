46a7fb5ad545a0b6410cb2f210df68e277cf879c
xiaofengfeng <feng826112@163.com>
2020-09-09 15:47:49 +0800

    登录和注册布局完成

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+$(function(){
+ 
+    $("#login_box").on("click",function(){
+        $(".login-box").hide()
+        $(".reg-box").show()
+    })
+
+    $("#reg_box").on("click",function(){
+        $(".reg-box").hide()
+        $(".login-box").show()
+    })
+
+
+
+})
