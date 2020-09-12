70ffe2274acd462fcaa7e0baf8d8c252d84bfe40
xiaofengfeng <feng826112@163.com>
2020-09-12 10:04:05 +0800

    页面权限验证

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ $(function(){
            type:'GET',
            
            success:function(res){
-               console.log(res);
+                
                if(res.status !==0){
                    return layer.msg('获取数据失败')
                }
@@ -16,6 +16,12 @@ $(function(){
                 
                //渲染数据到页面上
                renderData(res.data)
+           }
+           , complete:function(res){
+               if(res.responseJSON.status ===1 && res.responseJSON.message ==='身份认证失败！'){
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+               }
            }
        })
     }
